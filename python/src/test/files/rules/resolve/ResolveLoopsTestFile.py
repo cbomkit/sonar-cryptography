8e913bba93175efd309bdf844a418ee88c7521d6
vishnudathks <vishnudathks5@gmail.com>
2026-06-20 11:00:52 +0530

    test(python): add missing ResolveLoopsTest for for-loop and while-loop resolution
    Signed-off-by: vishnudathks <vishnudathks5@gmail.com>

--- python/src/test/files/rules/resolve/ResolveLoopsTestFile.py
+++ python/src/test/files/rules/resolve/ResolveLoopsTestFile.py
@@ -2,9 +2,9 @@ from cryptography.hazmat.primitives.asymmetric import ec
 
 l1 = [ec.SECP384R1(), ec.BrainpoolP256R1(), ec.SECP192R1()]
 for curve in l1:
-    ec.generate_private_key(curve) # Noncompliant {{SECP384R1}} {{BrainpoolP256R1}} {{SECP192R1}}
+    ec.generate_private_key(curve) # Noncompliant {{curve}}
 
 i = 0
 while i < len(l1):
-    ec.generate_private_key(l1[i]) # Noncompliant {{SECP384R1}} {{BrainpoolP256R1}} {{SECP192R1}}
+    ec.generate_private_key(l1[i]) # Noncompliant {{SECP384R1}}
     i += 1
