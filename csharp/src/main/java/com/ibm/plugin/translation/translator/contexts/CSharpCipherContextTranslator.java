4116fb0e76337c1cb064b1600613b75d9d382715
Fynn Thierling <fynnth@outlook.de>
2026-08-19 11:54:35 +0200

    feat(csharp): extend AES family detection with full operation coverage
    Ports the AES/AesGcm/AesCcm rule expansion from the aes-related-crypto
    
    reference branch: CreateEncryptor/CreateDecryptor, EncryptCbc/DecryptCbc,
    
    EncryptEcb/DecryptEcb, EncryptCfb/DecryptCfb, Try* variants, GenerateKey/
    
    GenerateIV, and AesGcm/AesCcm Encrypt/Decrypt operations as depending
    
    detection rules on the existing constructor/factory rules.
    
    Adds ModeFactory(String) to emit a constant Mode value when the mode is
    
    encoded in the method name (e.g. EncryptCbc) rather than a parameter, and
    
    wires ENCRYPT/DECRYPT/GENERATEKEY/GENERATEIV functionality translation
    
    into CSharpCipherContextTranslator.
    
    Signed-off-by: Fynn Thierling <fynnth@outlook.de>

--- csharp/src/main/java/com/ibm/plugin/translation/translator/contexts/CSharpCipherContextTranslator.java
+++ csharp/src/main/java/com/ibm/plugin/translation/translator/contexts/CSharpCipherContextTranslator.java
@@ -39,6 +39,10 @@ import com.ibm.mapper.model.algorithms.DES;
 import com.ibm.mapper.model.algorithms.DESede;
 import com.ibm.mapper.model.algorithms.RC2;
 import com.ibm.mapper.model.algorithms.RSA;
+import com.ibm.mapper.model.functionality.Decrypt;
+import com.ibm.mapper.model.functionality.Encrypt;
+import com.ibm.mapper.model.functionality.Generate;
+import com.ibm.mapper.model.functionality.KeyGeneration;
 import com.ibm.mapper.utils.DetectionLocation;
 import java.util.Optional;
 import javax.annotation.Nonnull;
@@ -68,6 +72,10 @@ public final class CSharpCipherContextTranslator
                                 Optional.of(new DESede(detectionLocation));
                         case "RSA" -> Optional.of(new RSA(detectionLocation));
                         case "RC2" -> Optional.of(new RC2(detectionLocation));
+                        case "ENCRYPT" -> Optional.of(new Encrypt(detectionLocation));
+                        case "DECRYPT" -> Optional.of(new Decrypt(detectionLocation));
+                        case "GENERATEKEY" -> Optional.of(new KeyGeneration(detectionLocation));
+                        case "GENERATEIV" -> Optional.of(new Generate(detectionLocation));
                         default -> Optional.empty();
                     };
             if (result.isPresent()) {
